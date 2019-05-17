35773fa4ff1701b2a0ec0900e4069842ecba3081
Bartek Krzys <bkrzys@clearcode.cc>
2019-05-17 18:38:40 +0200

    Refactor code: add helper renderer function to split responsibility of handling requests by the server and rendering react app

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -80,15 +80,9 @@ var _express = __webpack_require__(2);
 
 var _express2 = _interopRequireDefault(_express);
 
-var _react = __webpack_require__(0);
+var _renderer = __webpack_require__(3);
 
-var _react2 = _interopRequireDefault(_react);
-
-var _server = __webpack_require__(3);
-
-var _Home = __webpack_require__(4);
-
-var _Home2 = _interopRequireDefault(_Home);
+var _renderer2 = _interopRequireDefault(_renderer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -97,10 +91,7 @@ var app = (0, _express2.default)();
 app.use(_express2.default.static('public')); // treat public dir as static to the outside world
 
 app.get('/', function (req, resp) {
-  var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-
-  var html = '\n    <html>\n      <head></head>\n      <body>\n        <div id="root">' + content + '</div>\n        <script src="bundle.js"></script> \n      </body>\n    </html>\n  '; /** This <script> part will tell to download script from public/bundle.js **/
-  resp.send(html);
+  resp.send((0, _renderer2.default)());
 });
 
 app.listen(8080, function () {
@@ -115,12 +106,39 @@ module.exports = require("express");
 
 /***/ }),
 /* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(4);
+
+var _Home = __webpack_require__(5);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  return '\n    <html>\n      <head></head>\n      <body>\n        <div id="root">' + (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null)) + '</div>\n        <script src="bundle.js"></script> \n      </body>\n    </html>\n  ';
+};
+
+/***/ }),
+/* 4 */
 /***/ (function(module, exports) {
 
 module.exports = require("react-dom/server");
 
 /***/ }),
-/* 4 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,12 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
+import renderer from './helpers/renderer';
 
 const app = express();
 
 app.use(express.static('public')) // treat public dir as static to the outside world
 
 app.get('/', (req, resp) => {
-  const content = renderToString(<Home/>)
-  
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root">${content}</div>
-        <script src="bundle.js"></script> 
-      </body>
-    </html>
-  ` /** This <script> part will tell to download script from public/bundle.js **/
-  resp.send(html)
+  resp.send(renderer())
 })
 
 app.listen(8080, () => {
